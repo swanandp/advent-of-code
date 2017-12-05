87023592b9c228df2bc24188f1873c95eb648bc9
Swanand Pagnis <swanand.pagnis@gmail.com>
2017-12-05 11:37:11 +0530

    Initial commit

---
+++ lib/advent/of/code/version.rb
@@ -0,0 +1,7 @@
+module Advent
+  module Of
+    module Code
+      VERSION = "0.1.0"
+    end
+  end
+end
---
+++ lib/code.rb
@@ -0,0 +1,10 @@
+require "advent/of/code/version"
+require "advent/of/code/2017/day_1"
+
+module Advent
+  module Of
+    module Code
+      # Your code goes here...
+    end
+  end
+end
