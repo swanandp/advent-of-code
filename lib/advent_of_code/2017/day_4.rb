1e8b5f6c59f626c70a5a28af139faa7fbfa52904
Swanand Pagnis <swanand.pagnis@gmail.com>
2017-12-11 17:35:42 +0530

    Add Day4 part 2

--- lib/advent_of_code/2017/day_4.rb
+++ lib/advent_of_code/2017/day_4.rb
@@ -6,11 +6,22 @@ class Day4
   def initialize(phrases)
     self.input = phrases
     solve_part_1
+    solve_part_2
   end
 
   def solve_part_1
     self.output_part_1 = input.select do |phrase|
       phrase.length == Set.new(phrase).length
+    end.length
+  end
+
+  def solve_part_2
+    self.output_part_2 = input.select do |phrase|
+      sorted = phrase.map do |word|
+        word.split("").sort.join
+      end
+
+      sorted.length == Set.new(sorted).length
     end.length
   end
 
