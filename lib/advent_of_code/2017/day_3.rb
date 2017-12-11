9ad9b84d853a560516aa212b8f6b6335e690722d
Swanand Pagnis <swanand.pagnis@gmail.com>
2017-12-11 15:45:14 +0530

    Add a perfectly working neighbours function for D3

--- lib/advent_of_code/2017/day_3.rb
+++ lib/advent_of_code/2017/day_3.rb
@@ -3,16 +3,108 @@ class Day3
 
   def initialize(x)
     self.x = x
+    calculate_anchors!
     solve_part_1
-    # solve_part_2
+    solve_part_2
   end
 
   def solve_part_1
+    self.output_part_1 = center_distances.min + k
+  end
+
+  def solve_part_2
+
+  end
+
+  def neighbours
+    if (c, _ = corner?)
+      return corner_neighbours(corner_functions[c].(k - 1))[c]
+    end
+
+    edge_neighbours(* edge?)
+  end
+
+  def corner_neighbours(prev)
+    {
+      bottom_right: [x - 1, prev, prev + 1],
+      bottom_left: [x - 1, prev],
+      top_left: [x - 1, prev],
+      top_right: [x - 1, prev],
+    }
+  end
+
+  def edge_neighbours(edge, next_c, prev_c)
+    a = f_of_a(edge).(k - 1)
+
+    d_next = next_c - x
+    d_prev = x - prev_c
+
+    case edge
+    when :right
+      right_neighbours(a, d_next, d_prev)
+    else
+      other_neighbours(a, d_next, d_prev)
+    end
+  end
+
+  def right_neighbours(a, d_next, d_prev)
+    case d_next
+    when 1
+      [x - 1, a, a - 1]
+    else
+      case d_prev
+      when 1
+        [x - 1, a - d_next + 2]
+      when 2
+        [x - 1, x - 2, a - d_next + 1, a - d_next + 2]
+      else
+        [x - 1, a - d_next, a - d_next + 1, a - d_next + 2]
+      end
+    end
+  end
+
+  def other_neighbours(a, d_next, d_prev)
+    case d_next
+    when 1
+      [x - 1, a, a - 1]
+    else
+      case d_prev
+      when 1
+        [x - 1, x - 2, a - d_next + 1, a - d_next + 2]
+      else
+        [x - 1, a - d_next, a - d_next + 1, a - d_next + 2]
+      end
+    end
+  end
+
+  def corner?
+    corners.detect do |(_, n)|
+      n == x
+    end
+  end
+
+  # Return [edge_name, next_corner, prev_corner]
+  def edge?
+    return corner? if corner?
+
+    if corners[:top_left] > x && x > corners[:top_right]
+      [:top, corners[:top_left], corners[:top_right]]
+    elsif corners[:bottom_left] > x && x > corners[:top_left]
+      [:left, corners[:bottom_left], corners[:top_left]]
+    elsif corners[:bottom_right] > x && x > corners[:bottom_left]
+      [:bottom, corners[:bottom_right], corners[:bottom_left]]
+    elsif corners[:top_right] > x && x > corner_functions[:bottom_right].(k - 1)
+      [:right, corners[:top_right], corner_functions[:bottom_right].(k - 1)]
+    else
+      fail("Impossible case!")
+    end
+  end
+
+  def calculate_anchors!
     sqrt_ceil = Math.sqrt(x).ceil
     self.k2_1 = sqrt_ceil.odd? ? sqrt_ceil : 1 + sqrt_ceil
     self.k2_1_sq = k2_1 ** 2
     self.k = k2_1 / 2
-    self.output_part_1 = center_distances.min + k
   end
 
   def center_distances
@@ -26,5 +118,34 @@ class Day3
       k2_1_sq - 5 * k,
       k2_1_sq - 7 * k,
     ]
+  end
+
+  def corners
+    corner_functions.reduce({}) do |c, (l, f)|
+      c[l] = f.(k)
+      c
+    end
+  end
+
+  def corner_functions
+    {
+      bottom_right: ->(n) { (n * 2 + 1) ** 2 },
+      bottom_left: ->(n) { ((n * 2 + 1) ** 2) - 2 * n },
+      top_left: ->(n) { ((n * 2 + 1) ** 2) - 4 * n },
+      top_right: ->(n) { ((n * 2 + 1) ** 2) - 6 * n },
+    }
+  end
+
+  def f_of_a(edge)
+    case edge
+    when :right
+      corner_functions[:top_right]
+    when :top
+      corner_functions[:top_left]
+    when :left
+      corner_functions[:bottom_left]
+    else # :bottom
+      corner_functions[:bottom_right]
+    end
   end
 end
