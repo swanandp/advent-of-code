6d53da72151af444f5d331268ac0c06472142d78
Swanand Pagnis <swanand.pagnis@gmail.com>
2017-12-11 20:58:31 +0530

    Add Day 5 part 2

--- lib/advent_of_code/2017/day_5.rb
+++ lib/advent_of_code/2017/day_5.rb
@@ -5,6 +5,7 @@ class Day5
     self.original_instructions = instructions.freeze
     self.instructions = instructions.dup # clone not needed
     solve_part_1
+    solve_part_2
   end
 
   def solve_part_1
@@ -19,6 +20,27 @@ class Day5
     end
 
     self.output_part_1 = steps
+  end
+
+  def solve_part_2
+    self.instructions = original_instructions.dup
+    position = 0
+    steps = 0
+
+    while 0 <= position && position < original_instructions.length
+      n = instructions[position]
+
+      if n >= 3
+        instructions[position] -= 1
+      else
+        instructions[position] += 1
+      end
+
+      position += n
+      steps += 1
+    end
+
+    self.output_part_2 = steps
   end
 
   def self.from_file(file_path)
