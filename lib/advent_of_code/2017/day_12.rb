91f560798037b19288a3f7c4042552ddff932e3b
Swanand Pagnis <swanand.pagnis@gmail.com>
2017-12-19 17:39:18 +0530

    Day 12 Part 1
    Simple depth first graph traversal

---
+++ lib/advent_of_code/2017/day_12.rb
@@ -0,0 +1,37 @@
+class Day12
+  attr_accessor :graph
+
+  # {
+  #   0: [2],
+  #   1: [1],
+  #   2: [0, 3, 4],
+  #   1: [1],
+  # }
+  def initialize(graph)
+    self.graph = graph
+  end
+
+  def count(node, visited = {})
+    visited[node] = true
+
+    1 + graph[node].sum do |child|
+      next 0 if visited[child]
+
+      visited[child] = true
+      count(child, visited)
+    end
+  end
+
+  def self.from_file(file_path)
+    graph = {}
+
+    File.open(file_path, "r") do |f|
+      while (line = f.gets)
+        root, leaves = line.strip.split(" <-> ")
+        graph[root.to_i] = leaves.scanf("%d%s") { |(x, _)| x }
+      end
+    end
+
+    new(graph)
+  end
+end
