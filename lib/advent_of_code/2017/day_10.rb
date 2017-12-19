098e2b5dd0f08b271904d376dcb2b286864e516c
Swanand Pagnis <swanand.pagnis@gmail.com>
2017-12-19 10:38:57 +0530

    Day 10 part 2

--- lib/advent_of_code/2017/day_10.rb
+++ lib/advent_of_code/2017/day_10.rb
@@ -1,13 +1,16 @@
-class Day10
-  attr_accessor :current_position, :skip_size, :lengths, :list
+require "scanf"
 
-  def initialize(input_length, rotation_lengths)
+class Day10
+  attr_accessor :current_position, :skip_size, :lengths, :list,
+                :input, :sparse_hash, :dense_hash, :output_part_1, :output_part_2
+
+  def initialize(input_length, input)
     self.list = (0..(input_length || 255)).to_a #
-    self.lengths = rotation_lengths
+    self.lengths = input.scanf("%c") { |(x, _)| x.ord } + [17, 31, 73, 47, 23]
     self.current_position = 0
     self.skip_size = 0
 
-    solve_part_1
+    solve_part_2
   end
 
   def solve_part_1
@@ -16,6 +19,26 @@ class Day10
       self.current_position = (self.current_position + length + skip_size) % list.length
       self.skip_size += 1
     end
+
+    self.output_part_1 = list[0] * list[1]
+  end
+
+  def solve_part_2
+    64.times do
+      lengths.each do |length|
+        reverse_substring!(list, current_position, length)
+        self.current_position = (self.current_position + length + skip_size) % list.length
+        self.skip_size += 1
+      end
+    end
+
+    self.dense_hash = list.each_slice(16).map do |slice|
+      slice.reduce(0) { |xor, num| xor ^ num }
+    end
+
+    self.output_part_2 = dense_hash.map do |num|
+      num.to_s(16).rjust(2, "0")
+    end.join
   end
 
   def reverse_substring!(list, from_position, substring_length)
