1a573ecff5c21aeb57782c7064132c17f44d5303
Swanand Pagnis <swanand.pagnis@gmail.com>
2017-12-21 19:39:25 +0530

    Day 19 Part 1

---
+++ lib/advent_of_code/2017/day_19.rb
@@ -0,0 +1,96 @@
+class Day19
+  attr_accessor :tubes, :x, :y, :direction, :letters, :output_part_1
+
+  def initialize(tubes)
+    self.tubes = tubes
+
+    self.x = 0 # Always start in the first row
+    self.y = tubes[0].index("|")
+    self.direction = :down
+    self.letters = []
+  end
+
+  def solve_part_1
+    while valid?
+      move
+    end
+
+    self.output_part_1 = letters.join
+  end
+
+  def move
+    return unless valid?
+    step = tubes[x][y]
+
+    case step
+    when "|"
+      self.x, self.y = directions[direction].(x, y)
+    when "-"
+      self.x, self.y = directions[direction].(x, y)
+    when /[a-z]/i
+      self.letters << step
+      self.x, self.y = directions[direction].(x, y)
+    when "+"
+      self.direction, (self.x, self.y) = feasible_neighbours(self.x, self.y).first
+    else
+      fail("This shouldn't have happened: #{[x, y, step, direction]}".strip)
+    end
+  end
+
+  def valid?(x = self.x, y = self.y)
+    0 <= x && x < tubes.length &&
+      0 <= y && y < tubes[x].length &&
+      tubes[x][y] != " "
+  end
+
+  # WARNING Useful for changing directions, but otherwise introduces ambiguity
+  def feasible_neighbours(x, y)
+    # noinspection RubyArgCount
+    neighbours(x, y).select { |dir, (nx, ny)| # nx = neighbour's x
+      valid?(nx, ny) &&(dir != opposites[direction] || dir == direction)
+    }
+  end
+
+  def neighbours(x, y)
+    directions.reduce({}) do |m, (dir, l)|
+      m[dir] = l.(x, y)
+      m
+    end
+  end
+
+  def opposites
+    @opposites ||=
+      {
+        down: :up,
+        up: :down,
+        left: :right,
+        right: :left,
+      }
+  end
+
+  def directions
+    @directions ||=
+      {
+        down: ->(x, y) { [x + 1, y] },
+        up: ->(x, y) { [x - 1, y] },
+        left: ->(x, y) { [x, y - 1] },
+        right: ->(x, y) { [x, y + 1] },
+      }
+  end
+
+  def self.from_file(file_path)
+    new(parse_file(file_path))
+  end
+
+  def self.parse_file(file_path)
+    tubes = []
+
+    File.open(file_path, "r") do |f|
+      while (line = f.gets)
+        tubes << line.chomp
+      end
+    end
+
+    tubes
+  end
+end
