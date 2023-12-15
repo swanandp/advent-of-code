2bed2a94a1bae2f3b67720976bd3f736db252dca
Swanand Pagnis <swanand.pagnis@gmail.com>
2023-12-15 12:54:28 +0530

    Day 15 Part 1 & 2
    Day 14 Part 2 cleanup

--- lib/advent_of_code/2023/day14_parabolic_reflector_dish_part2.rb
+++ lib/advent_of_code/2023/day14_parabolic_reflector_dish_part2.rb
@@ -13,27 +13,19 @@ def sample_input
   INPUT
 end
 
-# j == 7, i == 0, dots = 0, matrix[i][j] = . -> dots = 1
-# j == 7, i == 1, dots = 1, matrix[i][j] = . -> dots = 2
-# j == 7, i == 2, dots = 2, matrix[i][j] = . -> dots = 3
-# j == 7, i == 3, dots = 3, matrix[i][j] = . -> dots = 4
-# j == 7, i == 4, dots = 4, matrix[i][j] = O -> load = 10 (10 - 4 + 4)
-# j == 7, i == 5, dots = 4, matrix[i][j] = # -> beam_length = 4, dots = 0
-# j == 7, i == 6, dots = 0, matrix[i][j] = . -> beam_length = 4, dots = 1
-# j == 7, i == 7, dots = 1, matrix[i][j] = O -> beam_length = 4, dots = 1, load =
-# j == 7, i == 8, dots =  , matrix[i][j] =   ->
-# j == 7, i == 9, dots =  , matrix[i][j] =   ->
-def total_load(input)
-  matrix = input.split("\n").map { |l| l.split("") }
-  beam_load = 0
+def print_matrix(matrix)
+  puts matrix.map { |l| l.join }.join("\n")
+end
 
+def tilt_north(matrix)
   matrix.length.times do |j|
     dots = 0
 
     matrix.first.length.times do |i|
       case matrix[i][j]
       when "O"
-        beam_load += (matrix.length - i + dots)
+        matrix[i][j] = "."
+        matrix[i - dots][j] = "O"
       when "."
         dots += 1
       when "#"
@@ -43,8 +35,94 @@ def total_load(input)
       end
     end
   end
+end
 
-  beam_load
+def tilt_west(matrix)
+  matrix.length.times do |i|
+    dots = 0
+
+    matrix.first.length.times do |j|
+      case matrix[i][j]
+      when "O"
+        matrix[i][j] = "."
+        matrix[i][j - dots] = "O"
+      when "."
+        dots += 1
+      when "#"
+        dots = 0
+      else
+        raise "Unhandled Input: #{matrix[i][j]} at [#{i},#{j}]"
+      end
+    end
+  end
+end
+
+def tilt_south(matrix)
+  matrix.length.times do |j|
+    dots = 0
+
+    matrix.first.length.times do |i|
+      case matrix[matrix.length - i - 1][j]
+      when "O"
+        matrix[matrix.length - i - 1][j] = "."
+        matrix[matrix.length - i - 1 + dots][j] = "O"
+      when "."
+        dots += 1
+      when "#"
+        dots = 0
+      else
+        raise "Unhandled Input: #{matrix[i][j]} at [#{i},#{j}]"
+      end
+    end
+  end
+end
+
+def tilt_east(matrix)
+  matrix.length.times do |i|
+    dots = 0
+
+    matrix.first.length.times do |j|
+      case matrix[i][matrix.length - j - 1]
+      when "O"
+        matrix[i][matrix.length - j - 1] = "."
+        matrix[i][matrix.length - j - 1 + dots] = "O"
+      when "."
+        dots += 1
+      when "#"
+        dots = 0
+      else
+        raise "Unhandled Input: #{matrix[i][j]} at [#{i},#{j}]"
+      end
+    end
+  end
+end
+
+def matrix_load(matrix)
+  load = 0
+
+  matrix.map.with_index { |r, i| r.count { |c| c == "O" } * (10 - i) }.sum
+
+  matrix.length.times do |i|
+    matrix.first.length.times do |j|
+      if matrix[i][j] == "O"
+        load += (matrix.length - i)
+      end
+    end
+  end
+
+  load
+end
+
+def total_load(input)
+  matrix = input.split("\n").map { |l| l.split("") }
+  tilt_cycle = %w[north west south east].cycle
+
+  1000.times.map do
+    tilt_cycle.take(4).each do |dir|
+      send("tilt_#{dir}".to_sym, matrix)
+    end
+    matrix.map.with_index { |r, i| r.count { |c| c == "O" } * (matrix.length - i) }.sum
+  end
 end
 
 pp total_load(sample_input)
